b05bc4b93b8ee41cbf089b12c2c329361396ea5f
wemrxiaobai <1473674817@qq.com>
2021-12-23 10:14:20 +0800

    node

--- src/compants/pcNode.js
+++ src/compants/pcNode.js
@@ -1,9 +1,13 @@
 
 const request =require('request');
 const iconv= require('iconv-lite');
-
-request('https://www.ygdy8.com/html/gndy/china/list_4_1.html',function(err, res,body){
+ 
+request('https://www.ygdy8.com/html/gndy/china/list_4_1.html',{ encoding: null },function(err, res,body){
     console.log(err);
     // console.log(res);
-    console.log(body);
+    // console.log(body);
+
+    const buffs=iconv.decode(body,'gb2312').toString('utf8');
+    console.log('------------',buffs);
+
 })
